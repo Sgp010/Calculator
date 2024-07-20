154c0dff075cad4cdc2ff7b710366adbd1ddaf66
Sgp010 <speetham10@gmail.com>
2024-07-20 15:10:02 +0530

    Add files via upload

--- Test.py
+++ Test.py
@@ -11,19 +11,25 @@ with st.container():
         b = st.number_input("input a number", value = None , key = "input2")
         st.write("Select an operator")
         left_column1, right_column1 = st.columns(2)
+        try:
+            # Ensure the inputs are not None before converting to int
+            num1 = int(a) if a else 0
+            num2 = int(b) if b else 0
+        except ValueError:
+            st.write("Please enter valid numbers")
         with left_column1:
             li = ["Addition", "Subtraction", "Multiplication", "Division", "Exponent"]
             sl = st.selectbox("---Choose an operator---", li)
             if sl == "Addition":
-                st.write(a+b)
+                st.write(num1 + num2)
             elif sl == "Subtraction":
-                st.write(a-b)
+                st.write(num1 - num2)
             elif sl == "Multiplication":
-                st.write(a*b)
+                st.write(num1 * num2)
             elif sl == "Division":
-                st.write(a/b)
+                st.write(num1 / num2)
             elif sl == "Exponent":
                 st.write(a,"to the power", b)
-                st.write(a**b)
+                st.write(num1 ** num2)
 
 
